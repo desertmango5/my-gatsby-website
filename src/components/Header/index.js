c0fc4efe4d5c0637a95ae6408fcb3a9a1b313b39
Michael Manges <michael.manges@gmail.com>
2018-01-24 14:38:34 +0400

    Add active class to navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import classnames from 'classnames'
+import classNames from 'classnames/bind'
 import style from './index.module.scss'
 
 class Header extends React.Component{
@@ -8,35 +8,112 @@ class Header extends React.Component{
     super();
     // bind functions to this
     this.click = this.click.bind(this)
+    this.clickAbout = this.clickAbout.bind(this)
+    this.clickBlog = this.clickBlog.bind(this)
+    this.clickPhotography = this.clickPhotography.bind(this)
 
     // states
-    this.state = { active: false }
+    this.state = { 
+      blogActive: false,
+      aboutActive: false,
+      photographyActive: false
+     }
   }
 
   // functions
+  clickAbout() {
+    this.setState({ 
+      blogActive: false,
+      aboutActive: true,
+      photographyActive: false
+    })
+  }
+
+  clickBlog() {
+    this.setState({ 
+      blogActive: true,
+      aboutActive: false,
+      photographyActive: false
+    })
+  }
+
+  clickPhotography() {
+    this.setState({ 
+      blogActive: false,
+      aboutActive: false,
+      photographyActive: true
+    })
+  }
+
   click() {
-    this.setState({ active: true })
+    this.setState({
+      blogActive: false,
+      aboutActive: false,
+      photographyActive: false
+    })
   }
 
   render() {
+    // bind style to 
+    let cx = classNames.bind(style)
+
+    const setClassesAbout = cx(
+      {
+        menu__item: true,
+        menu__active: this.state.aboutActive,
+      }
+    )
+
+    const setClassesBlog = cx(
+      {
+        menu__item: true,
+        menu__active: this.state.blogActive,
+      }
+    )
+
+    const setClassesPhotography = cx(
+      {
+        menu__item: true,
+        menu__active: this.state.photographyActive,
+      }
+    )
+
     return (
       <div className={style.navigation}>
         <div>
           <h1 className={style.title}>
-            <Link to="/" className={style.title__link}>{this.props.title}</Link>
+            <Link 
+              to="/" 
+              className={style.title__link}
+              onClick={this.click}
+              >
+                {this.props.title}
+              </Link>
           </h1>
           <h4 className={style.byline}>{this.props.byline}</h4>
         </div>
         <div className={style.menu}>
-          <Link to="/blog/" className={style.menu__item}>Blog</Link>
+          <Link 
+            to="/blog/" 
+            className={setClassesBlog}
+            onClick={this.clickBlog}
+            >
+              Blog
+            </Link>
           <Link 
             to="/about/" 
-            className={style.menu__item}
-            onClick={this.click}
+            className={setClassesAbout}
+            onClick={this.clickAbout}
           >
             About
           </Link>
-          <Link to="/photography/" className={style.menu__item}>Photography</Link>
+          <Link 
+            to="/photography/" 
+            className={setClassesPhotography}
+            onClick={this.clickPhotography}
+          >
+            Photography
+          </Link>
         </div>
       </div>
     )
