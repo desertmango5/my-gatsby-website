da79e1b6f2c3f3117c77036953be31d1741ab6a4
Michael Manges <michael.manges@gmail.com>
2018-01-25 15:51:33 +0400

    Format blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,7 +5,6 @@ import style from '../styles/blog.module.scss'
 
 const Blog = ({ data }) => (
   <div className={style.body}>
-    <h2>📚 BLOG 📚</h2>
     {data.allPosts.edges.map(post => (
       <div key={post.node.id} className={style.post}>
         <p className={style.date}>{post.node.date}</p>
@@ -13,23 +12,24 @@ const Blog = ({ data }) => (
           <h2 className={style.title}>{post.node.title}</h2>
         </Link>
         {post.node.authors.map(author => (
-          <div key={author.id}>
-            <p className={style.author}>Post by <span className={style.name}>{author.name}</span></p>
+          <div key={author.id} className={style.authorSection}>
+            <p className={style.author}>Posted by <span className={style.name}>{author.name}</span></p>
             <img src={author.image.url} alt={author.name} className={style.authorImage} />
           </div>
         ))}
-        <img 
-          src={post.node.coverImage.url} 
-          alt={post.node.title}
-          className={style.coverImage}  
-        />
-        <Markdown
-          source={post.node.content}
-          className={style.content}
-        />
+        <div className={style.main}>
+          <img 
+            src={post.node.coverImage.url} 
+            alt={post.node.title}
+            className={style.coverImage}  
+          />
+          <Markdown
+            source={post.node.content}
+            className={style.content}
+          />
+        </div>
       </div>
     ))}
-    <p>{data.allPosts.edges[0].node.title}</p>
   </div>
 )
 
@@ -37,7 +37,7 @@ export default Blog
 
 export const allPostsQuery = graphql`
   query allPosts {
-    allPosts {
+    allPosts(sort: {fields: [date], order: DESC}) {
       edges {
         node {
           id
