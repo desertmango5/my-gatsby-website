4e6befd323ee2e14c470347c261d7d2683279739
Michael Manges <michael.manges@gmail.com>
2018-02-05 22:08:25 +0400

    Update package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-postcss-sass`,
       options: {
