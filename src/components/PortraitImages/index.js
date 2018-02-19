045b02577794759947b9f8ad4cd6e0bf53e842d5
Michael Manges <michael.manges@gmail.com>
2018-02-19 20:07:30 +0400

    Add more portrait images to Photography

--- src/components/PortraitImages/index.js
+++ src/components/PortraitImages/index.js
@@ -20,8 +20,48 @@ const PortraitImages = () => (
       className={style.images__portrait}  
     />
     <img 
+      src={`https://res.cloudinary.com/${process.env.GATSBY_CLOUD_NAME}/image/upload/f_auto/v1518983987/mysite/memories.jpg`} 
+      alt="Backlight family posing in front of colorful sunset"
+      className={style.images__portrait}  
+    />
+    <img 
+      src={`https://res.cloudinary.com/${process.env.GATSBY_CLOUD_NAME}/image/upload/f_auto/v1519026501/mysite/family-8.jpg`} 
+      alt="Three young boys smiling"
+      className={style.images__portrait}  
+    />
+    <img 
+      src={`https://res.cloudinary.com/${process.env.GATSBY_CLOUD_NAME}/image/upload/f_auto/v1519026501/mysite/couple-15.jpg`} 
+      alt="Couple on a carpet in the desert"
+      className={style.images__portrait}  
+    />
+    <img 
+      src={`https://res.cloudinary.com/${process.env.GATSBY_CLOUD_NAME}/image/upload/f_auto/v1519026501/mysite/desert-couple.jpg`} 
+      alt="Couple holding hands in desert"
+      className={style.images__portrait}  
+    />
+    <img 
+      src={`https://res.cloudinary.com/${process.env.GATSBY_CLOUD_NAME}/image/upload/f_auto/v1519026501/mysite/throwing-sand.jpg`} 
+      alt="Man throwing sand in desert"
+      className={style.images__portrait}  
+    />
+    <img 
       src={`https://res.cloudinary.com/${process.env.GATSBY_CLOUD_NAME}/image/upload/v1517811454/mysite/woman.jpg`} 
       alt="Woman in blue dress lying on desert sand"
+      className={style.images__portrait}  
+    />
+    <img 
+      src={`https://res.cloudinary.com/${process.env.GATSBY_CLOUD_NAME}/image/upload/f_auto/v1519026501/mysite/family-5.jpg`} 
+      alt="Family in desert with long shadows"
+      className={style.images__portrait}  
+    />
+    <img 
+      src={`https://res.cloudinary.com/${process.env.GATSBY_CLOUD_NAME}/image/upload/f_auto/v1519026501/mysite/family-2.jpg`} 
+      alt="Family in desert"
+      className={style.images__portrait}  
+    />
+    <img 
+      src={`https://res.cloudinary.com/${process.env.GATSBY_CLOUD_NAME}/image/upload/f_auto/v1518983990/mysite/Derwin.jpg`} 
+      alt="Young man wearing a hat"
       className={style.images__portrait}  
     />
   </div>
